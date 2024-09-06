ac4e1dd1f556d5b7f166225baa37827fde0e1b5f
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-09-07 00:16:22 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWTxkgcyJrqzabhi5TjKfFD8Sr02XZl-Q",
-  authDomain: "socialex-85cb3.firebaseapp.com",
-  projectId: "socialex-85cb3",
-  storageBucket: "socialex-85cb3.appspot.com",
-  messagingSenderId: "682531867137",
-  appId: "1:682531867137:web:b9e07d8af4bcad5ba64e00"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
